c690671601ac7c346e999ba0ea511073e3cf731c
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-01 17:39:15 +0530

    added files

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+with st.expander("Start Camera"):
+    # Start the camera
+    camera_image = st.camera_input("Camera")
+
+if camera_image:
+    # Create a pillow image instance
+    img = Image.open(camera_image)
+
+    # Convert the pillow image to grayscale
+    gray_img = img.convert("L")
+
+    # Render the grayscale image on the webpage
+    st.image(gray_img)
